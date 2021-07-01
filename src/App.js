a0af2f369dc491c40a8bf65e09be75d5d1b0a28a
Jelani Canty <jelani.canty@peapoddigitallabs.com>
2021-06-30 22:19:19 -0500

    more weather api data

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ const weather_map_api = {
   key: "69f5622ea693e1adfea3c6407207bb9f",
   base: "https://api.openweathermap.org/data/2.5/"
 }
-
 const thingspeak_api = {
   key: "6OKMFXMALH1QI7J4",
   base: "https://api.thingspeak.com/channels/1411578/feeds.json"
 }
-
-
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
-
   console.log(query);
-
   if (query === "Chicago") {
     fetch(`${thingspeak_api.base}?api_key=${thingspeak_api.key}&results=5`)
       .then(res => res.json())
       .then(result => console.log(result))
   }
-
   const search = evt => {
     if (evt.key === "Enter") {
       fetch(`${weather_map_api.base}weather?q=${query}&units=imperial&APPID=${weather_map_api.key}`)
@@ -33,19 +27,15 @@ function App() {
         });
     }
   }
-
   const dateBuilder = (d) => {
     let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
     let day = days[d.getDay()];
     let date = d.getDate();
     let month = months[d.getMonth()];
     let year = d.getFullYear();
-
     return `${day} ${date} ${month} ${year}`
   }
-
   return (
     <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
       <main>
@@ -59,7 +49,6 @@ function App() {
             onKeyPress={search}
           />
         </div>
-
         {(typeof weather.main != "undefined") ? (
           <div>
             <h1>Live Updates from OpenWeatherMap.com</h1>
@@ -70,7 +59,28 @@ function App() {
             <div className="weather-box">
               <div className="temp">
                 {Math.round(weather.main.temp)}°F
-           </div>
+         </div>
+              <div className="temp_min">
+                <h4>Min Temperature : </h4>{Math.round(weather.main.temp_min)}°F
+         </div>
+
+
+              <div className="temp_max">
+                <h4>Max temperature : </h4>{Math.round(weather.main.temp_max)}°F
+         </div>
+              <div className="humidity">
+                <h4>Humidity : </h4>{Math.round(weather.main.humidity)}%
+         </div>
+              <div className="pressure">
+                <h4>Pressure : </h4>{Math.round(weather.main.pressure)}
+              </div>
+              <div className="speed">
+                <h4>Wind Speed: </h4>{Math.round(weather.wind.speed)}mph
+         </div>
+              <div className="gust">
+                <h4>Wind gust: </h4>{Math.round(weather.wind.gust)}mph
+         </div>
+
               <div className="weather">{weather.weather[0].main}</div>
             </div>
           </div>
